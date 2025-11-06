fbbb3cac9cc766dee1886b630f7d381c0b2a35cb
Neville Brem <contact@nevillebrem.com>
2025-11-07 00:07:46 +0100

    simplified tool handling for now (might do batching later)

--- src/lib/components/ToolWrapper.svelte
+++ src/lib/components/ToolWrapper.svelte
@@ -1,9 +1,17 @@
 <script lang="ts">
-	import type { Snippet } from 'svelte';
+	import { ToolCase, type IconProps } from '@lucide/svelte';
+	import type { Component, Snippet } from 'svelte';
 
-	let { children }: { children: Snippet } = $props();
+	let {
+		children,
+		icon,
+		className
+	}: { children: Snippet; icon?: Component<IconProps>; className?: string } = $props();
+
+	const Icon = icon || ToolCase;
 </script>
 
-<p class="my-2 flex items-center gap-2 text-muted-foreground select-none">
+<p class={['my-2 flex items-center gap-2 text-muted-foreground select-none', className]}>
+	<Icon size={14} />
 	{@render children?.()}
 </p>
