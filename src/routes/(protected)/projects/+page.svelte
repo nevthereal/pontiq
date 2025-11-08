80319c7de628ffd0864c0f261451eccec164fe15
Neville Brem <contact@nevillebrem.com>
2025-11-08 09:21:42 +0100

    UI improvements

--- src/routes/(protected)/projects/+page.svelte
+++ src/routes/(protected)/projects/+page.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
 	import * as Select from '$lib/components/ui/select/index.js';
 	import Input from '$lib/components/ui/input/input.svelte';
-	import Label from '$lib/components/ui/label/label.svelte';
 	import * as Field from '$lib/components/ui/field/index.js';
 	import * as Item from '$lib/components/ui/item';
 	import { resolve } from '$app/paths';
@@ -104,10 +103,12 @@
 		</Dialog.Root>
 	</div>
 	<svelte:boundary>
-		{#each await getSubjectsWithProjects() as sub (sub.id)}
+		{@const subs = await getSubjectsWithProjects()}
+		<h2 class="mb-2 text-xl font-medium text-muted-foreground">Pinned Subjects</h2>
+		{#each subs.filter((s) => s.pinned === true) as sub (sub.id)}
 			<div class="mb-2">
 				<h1 class="mb-2 text-xl">{sub.title}:</h1>
-				<Item.Group class="grid grid-cols-4 gap-2">
+				<Item.Group class="grid gap-2 md:grid-cols-2 xl:grid-cols-4">
 					{#each sub.projects as prj (prj.id)}
 						<Item.Root variant="outline">
 							{#snippet child({ props })}
@@ -126,6 +127,34 @@
 					{/each}
 				</Item.Group>
 			</div>
+		{:else}
+			<Muted>No pinned subjects</Muted>
+		{/each}
+		<h2 class="mb-2 text-xl font-medium text-muted-foreground">Other Subjects</h2>
+		{#each subs.filter((s) => s.pinned === false) as sub (sub.id)}
+			<div class="mb-2">
+				<h1 class="mb-2 text-xl">{sub.title}:</h1>
+				<Item.Group class="grid gap-2 md:grid-cols-2 xl:grid-cols-4">
+					{#each sub.projects as prj (prj.id)}
+						<Item.Root variant="outline">
+							{#snippet child({ props })}
+								<a
+									href={resolve(`/(protected)/projects/[project_id]`, { project_id: prj.id })}
+									{...props}
+								>
+									<Item.Title>
+										{prj.name}
+									</Item.Title>
+								</a>
+							{/snippet}
+						</Item.Root>
+					{:else}
+						<Muted>No projects in subject</Muted>
+					{/each}
+				</Item.Group>
+			</div>
+		{:else}
+			<Muted>No subjects. Create one above!</Muted>
 		{/each}
 
 		{#snippet pending()}
