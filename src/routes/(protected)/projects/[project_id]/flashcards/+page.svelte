cd03781bf3e56f50983dcb4e75fe042847688c62
Neville Brem <contact@nevillebrem.com>
2026-02-08 12:52:38 +0100

    revamped next/submission menu

--- src/routes/(protected)/projects/[project_id]/flashcards/+page.svelte
+++ src/routes/(protected)/projects/[project_id]/flashcards/+page.svelte
@@ -4,7 +4,9 @@
 	import Button from '$lib/components/ui/button/button.svelte';
 
 	import { getFlashCards } from '$lib/remote/tools.remote';
-	import { ArrowLeft, ArrowRight, CreditCard } from '@lucide/svelte';
+	import { CreditCard, Frown, Laugh, Meh, Smile } from '@lucide/svelte';
+	import { cubicInOut } from 'svelte/easing';
+	import { fade } from 'svelte/transition';
 
 	let { params } = $props();
 
@@ -12,6 +14,11 @@
 
 	let currentIndex = $state(0);
 	let flipped = $state(false);
+
+	function handleSubmission(index) {
+		currentIndex++;
+		flipped = false;
+	}
 </script>
 
 <div>
@@ -29,29 +36,25 @@
 				bind:flipped
 			/>
 
-			<div class="flex justify-between">
-				{#if currentIndex != 0}
-					<Button
-						variant="ghost"
-						size="icon-lg"
-						onclick={() => {
-							currentIndex--;
-							flipped = false;
-						}}><ArrowLeft /></Button
-					>
-				{/if}
-				{#if currentIndex + 1 != flashcards.length}
-					<Button
-						variant="ghost"
-						size="icon-lg"
-						class="ml-auto"
-						onclick={() => {
-							currentIndex++;
-							flipped = false;
-						}}><ArrowRight /></Button
-					>
-				{/if}
-			</div>
+			{#if flipped}
+				<div transition:fade={{ duration: 100, easing: cubicInOut }}>
+					<h1 class="mb-2 text-center font-bold">How well could you recall this flashcard?</h1>
+					<div class="grid grid-cols-4 gap-4">
+						<Button onclick={() => handleSubmission(1)} variant="outline"
+							><Frown class="text-red-400" />Not at all</Button
+						>
+						<Button onclick={() => handleSubmission(2)} variant="outline"
+							><Meh class="text-orange-400" />Struggling</Button
+						>
+						<Button onclick={() => handleSubmission(3)} variant="outline"
+							><Smile class="text-green-400" />Got it!</Button
+						>
+						<Button onclick={() => handleSubmission(4)} variant="outline"
+							><Laugh class="text-emerald-400" />Very Fast</Button
+						>
+					</div>
+				</div>
+			{/if}
 		{/if}
 	</div>
 </div>
